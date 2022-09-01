0382767ef5627a4a8b239c82705ba4ee50efdd91
maui-dev <108886458+Jamesyun123@users.noreply.github.com>
2022-09-01 10:14:04 +0900

    starzly

---
+++ src/pages/Landing.js
@@ -0,0 +1,412 @@
+import React from 'react';
+
+const Landing = () => {
+	return (
+		<>
+			<div class="container mt-5">
+				<div class="row pt-5">
+					<div class="col-sm-7 mt-5 px-5">
+						<div class="row pt-5">
+							<div class="col-12">
+								<span class="starzly-title">STARZLY</span>
+							</div>
+						</div>
+						<div class="row h-25 ">
+							<div class="col-12">
+								<span class="starzly-font-futura-20-nocenter">
+									BRINGS THE BEST INTERACTIVE VIDEO-POWERED
+									SHOPPING EXPERIENCE TO YOUR POCKET!
+								</span>
+							</div>
+						</div>
+						<div class="row w-100 pt-5">
+							<div class="col-12">
+								<span class="starzly-font-Helve">
+									THE NEXT GENERATION OF SHOPPING.
+								</span>
+							</div>
+						</div>
+						<div class="row w-100 h-50 pt-2">
+							<div class="col">
+								<img class="starzly-button-size" src="./assets/image/group-14.png" />
+								<img class="starzly-button-size mt-1" src="./assets/image/group-12.png" />
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-5">
+						<div class="row">
+							<div class="col-12">
+								<video width="300px" height="480px" controls autoplay>
+									<source src="/Video/DialSticker.mp4" type="video/mp4" />
+									Sorry, your browser doesn't support videos.
+								</video>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row mt-5">
+					<span class="starzly-font-futura-20-center pt-5">
+						THE PERFECT PRODUCTS BASED ON
+						<br />
+						REAL RECOMMENDATIONS FROM REAL PEOPLE
+					</span>
+				</div>
+				<div class="row mt-5">
+					<div class="col-sm-4">
+						<div class="row">
+							<div class="col d-flex align-items-center justify-content-center">
+								<img src="./assets/image/istockphoto-1010215852-612-x-612-removebg-preview.png" width="120px" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="col starzly-font-18-Helve-center">
+								<span>
+									Have fun watching shoppable video
+								</span>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col starzly-font-15-Helve-center">
+								<span >
+									Discover the perfect products
+									based on an endless stream of
+									videos of celebrities and the
+									people you love reviewing their
+									favourite song.
+								</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="row">
+							<div class="col d-flex align-items-center justify-content-center">
+								<img src="./assets/image/istockphoto-1010215852-612-x-612-removebg-preview.png" width="120px" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="col starzly-font-18-Helve-center">
+								<span>
+									Have fun watching shoppable video
+								</span>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col starzly-font-15-Helve-center">
+								<span >
+									Discover the perfect products
+									based on an endless stream of
+									videos of celebrities and the
+									people you love reviewing their
+									favourite song.
+								</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="row">
+							<div class="col d-flex align-items-center justify-content-center">
+								<img src="./assets/image/istockphoto-1010215852-612-x-612-removebg-preview.png" width="120px" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="col starzly-font-18-Helve-center">
+								<span>
+									Have fun watching shoppable video
+								</span>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col starzly-font-15-Helve-center">
+								<span >
+									Discover the perfect products
+									based on an endless stream of
+									videos of celebrities and the
+									people you love reviewing their
+									favourite song.
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row mt-5">
+					<span class="col-12 pt-5 starzly-font-futura-20-center">
+						ALL YOU NEED TO DO IS:
+					</span>
+				</div>
+				<div class="row starzly-rectangle mt-5">
+					<div class="col-sm-2 d-flex align-items-center justify-content-center">
+						<img src="./assets/image/group-5.png" />
+					</div>
+					<div class="col-sm-6">
+						 <div class="row pt-4">
+								<div class="col">
+									<span class="starzly-font-futura-16-nocenter">
+										Discover new products
+									</span>
+								</div>
+						 </div>
+						 <div class="row pb-4">
+								<div class="col">
+									<span class="starzly-font-15-Helve-nocenter">
+										Browse, watch and enjoy the endless stream of 
+										entertaining product video reviews created by 
+										people you know, like, and trust.
+									</span>
+								</div>
+						 </div>
+					</div>
+					<div class="col-sm-4 d-flex align-items-center justify-content-center">
+						<img src="./assets/image/istockphoto-1010215852-612-x-612-removebg-preview.png" width="120px" />
+					</div>
+				</div>
+				<div class="row starzly-rectangle mt-3">
+					<div class="col-sm-4 d-flex align-items-center justify-content-center">
+						<img src="./assets/image/istockphoto-1010215852-612-x-612-removebg-preview.png" width="120px" />
+					</div>
+					<div class="col-sm-2 d-flex align-items-center justify-content-center">
+						<img src="./assets/image/group-5.png" />
+					</div>
+					<div class="col-sm-6">
+						 <div class="row pt-4">
+								<div class="col">
+									<span class="starzly-font-futura-16-nocenter">
+										Discover new products
+									</span>
+								</div>
+						 </div>
+						 <div class="row pb-4">
+								<div class="col">
+									<span class="starzly-font-15-Helve-nocenter">
+										Browse, watch and enjoy the endless stream of 
+										entertaining product video reviews created by 
+										people you know, like, and trust.
+									</span>
+								</div>
+						 </div>
+					</div>
+				</div>
+				<div class="row starzly-rectangle mt-3">
+					<div class="col-sm-2 d-flex align-items-center justify-content-center">
+						<img src="./assets/image/group-5.png" />
+					</div>
+					<div class="col-sm-6">
+						 <div class="row pt-4">
+								<div class="col">
+									<span class="starzly-font-futura-16-nocenter">
+										Discover new products
+									</span>
+								</div>
+						 </div>
+						 <div class="row pb-4">
+								<div class="col">
+									<span class="starzly-font-15-Helve-nocenter">
+										Browse, watch and enjoy the endless stream of 
+										entertaining product video reviews created by 
+										people you know, like, and trust.
+									</span>
+								</div>
+						 </div>
+					</div>
+					<div class="col-sm-4 d-flex align-items-center justify-content-center">
+						<img src="./assets/image/istockphoto-1010215852-612-x-612-removebg-preview.png" width="120px" />
+					</div>
+				</div>
+				<div class="row mt-5">
+					<div class="col-12 pt-5">
+						<div class="row">
+							<span class="starzly-font-futura-20-center">
+								ELEVATE YOUR BRAND WITH STARZLY
+							</span>
+						</div>
+						<div class="row">
+							<span class="starzly-font-15-Helve-center">
+								Become a brand partner and benefit from:
+							</span>
+						</div>
+					</div>
+				</div>
+				<div class="row mt-5">
+					<div class="col-md-6 col-sm-12">
+						<div class="starzly-rectangle">
+							<div class="row pt-4">
+								<div class="col-sm-3 pt-3 ps-5">
+									<img src="./assets/image/bitmap(1).png" width="30px" />
+								</div>
+								<div class="col-sm-9">
+									<div class="row">
+										<div class="col">
+											<span class="starzly-font-futura-12-nocenter">
+												BOOST BRAND
+												<br />
+												AWARENESS
+											</span>
+										</div>
+									</div>
+									<div class="row pe-3">
+										<div class="col">
+											<span class="starzly-font-15-Helve-nocenter">
+												Connect your brand with celebrities and buyers through
+												an interview video-powered shopping experience.
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row pt-3">
+								<div class="col-sm-3 pt-3 ps-5">
+									<img src="./assets/image/bitmap(1).png" width="30px" />
+								</div>
+								<div class="col-sm-9">
+									<div class="row">
+										<div class="col">
+											<span class="starzly-font-futura-12-nocenter">
+												BOOST BRAND
+												<br />
+												AWARENESS
+											</span>
+										</div>
+									</div>
+									<div class="row pe-3">
+										<div class="col">
+											<span class="starzly-font-15-Helve-nocenter">
+												Connect your brand with celebrities and buyers through
+												an interview video-powered shopping experience.
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row pt-3">
+								<div class="col-sm-3 pt-3 ps-5">
+									<img src="./assets/image/bitmap(1).png" width="30px" />
+								</div>
+								<div class="col-sm-9">
+									<div class="row">
+										<div class="col">
+											<span class="starzly-font-futura-12-nocenter">
+												BOOST BRAND
+												<br />
+												AWARENESS
+											</span>
+										</div>
+									</div>
+									<div class="row pe-3">
+										<div class="col">
+											<span class="starzly-font-15-Helve-nocenter">
+												Connect your brand with celebrities and buyers through
+												an interview video-powered shopping experience.
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row mt-5 mb-5">
+					<div class="pt-5"></div>
+					<div class="col starzly-rectangle px-4 pb-3">
+						<div class="row pt-4">
+							<div class="col starzly-font-futura-20-center">
+								<span>
+									DOWNLOAD STARZLY NOW
+								</span>
+							</div>
+						</div>
+						<div class="row mt-2">
+							<div class="col-12 starzly-font-15-Helve-center">
+								<span>
+									Have fun, shop for your favourite products, and make
+									money-all in one video-powered marketplace.
+								</span>
+							</div>
+						</div>
+						<div class="mt-3 starzly-rec" >
+							<div class="starzly-video-rectangle"></div>
+							<div class="starzly-video-rectangle"></div>
+							<div class="starzly-video-rectangle"></div>
+							<div class="starzly-video-rectangle"></div>
+							<div class="starzly-video-rectangle"></div>
+							<div class="starzly-video-rectangle"></div>
+							<div class="starzly-video-rectangle"></div>
+						</div>
+						<div class="row mt-3">
+							<div class="col d-flex align-items-center justify-content-center">
+								<img class="starzly-button-size" src="./assets/image/group-14.png" />
+							  <img class="starzly-button-size" src="./assets/image/group-12.png" />
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row mt-5 starzly-font-15-Helve-nocenter">
+					<div class="col pt-5">
+						<div class="row">
+							<div class="col-sm-5">
+								<span>
+									Term of Service
+								</span>
+							</div>
+							<div class="col-sm-4 text-danger">
+								<span>
+									Privacy Policy
+								</span>
+							</div>
+							<div class="col-sm-3">
+								<span>
+									Contact us
+								</span>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col">
+								<span class="text-muted">
+									Get updates about our latest talents and news
+								</span>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-8">
+								<form>
+        						<input type="text" placeholder="yourname@youemail.com"/>
+   				 			</form>
+							</div>
+							<div class="col-4">
+								<img class="starzly-button-size" src="./assets/image/group-17.png" />
+							</div>
+						</div>
+					</div>
+					<div class="col pt-5"></div>
+					<div class="col pt-5">
+						<div class="row">
+							<div class="col">
+								<img src="./assets/image/facebook-circular-logo.png"/>
+							</div>
+							<div class="col">
+								<img src="./assets/image/instagram-logo.png"/>
+							</div>
+							<div class="col">
+								<img src="./assets/image/apple.png"/>
+							</div>
+							<div class="col">
+								<img src="./assets/image/google-play.png"/>
+							</div>
+							<div class="col">
+								<img src="./assets/image/twitter-social-logotype.png"/>
+							</div>
+						</div>
+						<div class="row">
+							<span>
+								You can become a Star in Starzly.
+							</span>
+						</div>
+						<div class="row">
+							<img class="starzly-button-size" src="./assets/image/group-17.png" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Landing;
